976a7c4b14e89805fc5ed57c3ae3f2c9f177c4c9
Hadrien Bertrand <hadrien.bertrand@mila.quebec>
2019-06-04 11:04:24 -0400

    ADD: data loader with labels

--- code/data.py
+++ code/data.py
@@ -5,25 +5,18 @@ import tensorflow as tf
 
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def preprocess_image(image):
-    image = tf.image.decode_image(image)
-    print(image.shape)
-    print(image.dtype)
-    print(np.unique(image.numpy()))
+    image = tf.io.decode_jpeg(image, channels=3)
     image = tf.cast(image, tf.float32)
     image /= 255.0  # normalize to [0,1] range
-
     return image
 
 
 def load_and_preprocess_image(path):
     image = tf.io.read_file(path)
-    print(path)
-    img = plt.imread(path)
-    plt.imshow(img)
-    plt.show()
     return preprocess_image(image)
 
 
@@ -37,41 +30,54 @@ class AmazonDataset:
                     continue
                 self.samples.append((row[0], join(image_dir, '{}.jpg'.format(row[0])), row[1].split(' ')))
 
-        # Check all images exists
+        # Check all images exists and create labels list
+        self.labels = []
         for sample in self.samples:
             if not exists(sample[1]):
                 print("WARNING: {} does not exist".format(sample[1]))
+
+            for label in sample[2]:
+                if label not in self.labels:
+                    self.labels.append(label)
+
+        self.mb = MultiLabelBinarizer(classes=self.labels)
+        self.mb.fit(self.labels)
+
+        # Transform all labels
+        self.transformed_labels = []
+        for sample in self.samples:
+            self.transformed_labels.append(self.mb.transform([sample[2]]).squeeze())
 
     def __len__(self):
         return len(self.samples)
 
     def get_tf_version(self):
         img_ds = tf.data.Dataset.from_tensor_slices([s[1] for s in self.samples])
-        image = load_and_preprocess_image([s[1] for s in self.samples][0])
+        img_ds = img_ds.map(load_and_preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+        label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(self.transformed_labels, tf.int64))
+
+        img_label_ds = tf.data.Dataset.zip((img_ds, label_ds))
+
+        return img_label_ds
+
+
+if __name__ == "__main__":
+    image_dir = '/home/hadrien/Downloads/rainforest/fixed-train-jpg/'
+    labels_csv = '/home/hadrien/Downloads/rainforest/train_v2.csv'
+    dataset = AmazonDataset(image_dir, labels_csv)
+
+    print("Dataset len: {}".format(len(dataset)))
+    ds = dataset.get_tf_version()
+
+    for n, sample in enumerate(ds.take(4)):
+        image, label = sample[0], sample[1]
+        image = image.numpy()
+        print(image.shape)
+        print(np.unique(image))
         plt.imshow(image)
         plt.grid(False)
         plt.xticks([])
         plt.yticks([])
         plt.show()
         plt.close()
-        img_ds = img_ds.map(load_and_preprocess_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-
-        for n, image in enumerate(img_ds.take(4)):
-            image = image.numpy()
-            print(image.shape)
-            print(np.unique(image))
-            plt.imshow(image)
-            plt.grid(False)
-            plt.xticks([])
-            plt.yticks([])
-            plt.show()
-            plt.close()
-
-
-if __name__ == "__main__":
-    image_dir = '/home/hadrien/Downloads/rainforest/train-jpg/'
-    labels_csv = '/home/hadrien/Downloads/rainforest/train_v2.csv'
-    dataset = AmazonDataset(image_dir, labels_csv)
-
-    print(len(dataset))
-    dataset.get_tf_version()
