e16cc7fc3b01875537ae3d350f90d2cdfed32e25
Simon Blackburn <simon.blackburn@mila.quebec>
2019-06-04 15:05:34 -0400

    Add keras model.fit()

--- code/models.py
+++ code/models.py
@@ -2,15 +2,18 @@ import tensorflow as tf
 
 
 class testMLP(tf.keras.Model):
-    def __init__(self, insize, hiddensize, outsize):
+    def __init__(self, hiddensize, outsize):
         super(testMLP, self).__init__(self)
         self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(hiddensize, input_shape=(insize,), activation=tf.nn.relu),
+            #tf.keras.layers.Reshape((256*256*3,)),
+            tf.keras.layers.Dense(hiddensize, input_shape=(256*256*3,), activation=tf.nn.relu),
             tf.keras.layers.Dense(outsize, activation=tf.nn.softmax)
         ])
     
     def call(self, inputs):
+        print(inputs)
         return self.model(inputs)
+
 
 if __name__=="__main__":
     testmodel = testMLP(5, 10, 2)
--- code/train.py
+++ code/train.py
@@ -1,27 +1,36 @@
 import tensorflow as tf
 import numpy as np
 import models
-from data import AmazonDataset
+import data
 
 
 def train_loop(dataset, model, optimizer):
-    for x in dataset.img_ds.batch(4):
-        gt = np.zeros((x.shape[0], 3))
-        gt[:,0] = 1
-        gt = tf.convert_to_tensor(gt)  
+    for x, y in dataset.batch(5):
         with tf.GradientTape() as tape:
-            predictions = model(tf.reshape(x, [x.shape[0], -1]))
-            loss = tf.keras.metrics.categorical_crossentropy(gt, predictions)
+            predictions = model(x)
+            loss = tf.keras.metrics.MSE(y, predictions)
         gradients = tape.gradient(loss, model.trainable_variables)        
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        print(loss)
+
+
+def fit_loop(dataset, model):
+    dataset.batch(4)
+    model.compile(optimizer=tf.keras.optimizers.Adam(),
+                  loss="MSE",
+                  metrics=["accuracy"])
+    print("Dataset", dataset)
+    model.fit(dataset, epochs=20, steps_per_epoch=2)
+
 
 if __name__=="__main__":
-    image_dir = 'rainforest/fixed-train-jpg/'
-    labels_csv = 'rainforest/train_v3.csv'
-    dataset = AmazonDataset(image_dir, labels_csv)
-    model = models.testMLP(256*256*3, 10, 3)
+    image_dir = '../../rainforest/fixed-train-jpg/'
+    labels_csv = '../../rainforest/train_v3.csv'
+    dataset = data.get_dataset(image_dir, labels_csv)
+    model = models.testMLP(10, 9)
     optimizer = tf.keras.optimizers.Adam(lr=0.001)
+    fit_loop(dataset, model)
     nepoch = 2 
-    for _ in range(nepoch):
-        train_loop(dataset, model, optimizer)
+    #for _ in range(nepoch):
+    #    train_loop(dataset, model, optimizer)
     
