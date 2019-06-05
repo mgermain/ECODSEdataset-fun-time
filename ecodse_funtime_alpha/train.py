5b359b70c8068653c005f9f7efb55fc21065f8d5
sblackburn86 <35506852+sblackburn86@users.noreply.github.com>
2019-06-05 17:00:32 -0400

    ADD: simple CNN and training loop
    * Add keras model.fit()
    
    * fix pep8
    
    * Fix capitalization of class name
    
    * Update train.py
    
    * adding a simple CNN for testing
    
    * cleaning branch
    
    * fixing flake8 error
    
    * moar flake8 fixes

--- ecodse_funtime_alpha/train.py
+++ ecodse_funtime_alpha/train.py
@@ -1,27 +1,36 @@
 import tensorflow as tf
-import numpy as np
 import models
-from data import AmazonDataset
+import data
 
 
 def train_loop(dataset, model, optimizer):
-    for x in dataset.img_ds.batch(4):
-        gt = np.zeros((x.shape[0], 3))
-        gt[:, 0] = 1
-        gt = tf.convert_to_tensor(gt)
+    for x, y in dataset.batch(5):
         with tf.GradientTape() as tape:
-            predictions = model(tf.reshape(x, [x.shape[0], -1]))
-            loss = tf.keras.metrics.categorical_crossentropy(gt, predictions)
+            predictions = model(tf.squeeze(x))
+            loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.cast(y, tf.float32), logits=predictions)
         gradients = tape.gradient(loss, model.trainable_variables)
         optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        print(loss)
+
+
+def fit_loop(dataset, model, optimizer):
+    dataset = dataset.shuffle(12)
+    dataset = dataset.repeat(20)
+    dataset = dataset.batch(4)
+    model.compile(optimizer=optimizer,
+                  loss=tf.keras.losses.binary_crossentropy,
+                  metrics=["accuracy"])
+    model.fit(dataset, epochs=20, steps_per_epoch=2)
 
 
 if __name__ == "__main__":
-    image_dir = 'rainforest/fixed-train-jpg/'
-    labels_csv = 'rainforest/train_v3.csv'
-    dataset = AmazonDataset(image_dir, labels_csv)
-    model = models.testMLP(256 * 256 * 3, 10, 3)
+    image_dir = '../../rainforest/fixed-train-jpg/'
+    labels_csv = '../../rainforest/train_v3.csv'
+    dataset = data.get_dataset(image_dir, labels_csv)
+    # model = models.TestMLP(10, 9)
+    model = models.SimpleCNN(10, 3, 9)
     optimizer = tf.keras.optimizers.Adam(lr=0.001)
-    nepoch = 2
-    for _ in range(nepoch):
-        train_loop(dataset, model, optimizer)
+    fit_loop(dataset, model, optimizer)
+    # nepoch = 2
+    # for _ in range(nepoch):
+    #     train_loop(dataset, model, optimizer)
