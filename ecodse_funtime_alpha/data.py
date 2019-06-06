9fe5e42bf11a0d2edcfa54fdfe328c7be140a23c
Hadrien Bertrand <hadrien.bertrand@mila.quebec>
2019-06-06 16:14:22 -0400

    DOC: documented all functions

--- ecodse_funtime_alpha/data.py
+++ ecodse_funtime_alpha/data.py
@@ -3,13 +3,24 @@ from os.path import join, exists
 
 import tensorflow as tf
 
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import MultiLabelBinarizer
 
 tf.enable_eager_execution()
 
 
 def preprocess_image(image):
+    """
+    Decode a jpeg and normalize it to [0, 1] range.
+
+    Parameters
+    ----------
+    image : tf tensor
+        Raw image as a tf tensor
+
+    Returns
+    -------
+        tf tensor in float32
+    """
     image = tf.io.decode_jpeg(image, dct_method="INTEGER_ACCURATE")
     image = tf.cast(image, tf.float32)
     image /= 255.0  # normalize to [0,1] range
@@ -17,11 +28,38 @@ def preprocess_image(image):
 
 
 def load_and_preprocess_image(path):
+    """
+    Read a jpeg and normalize it to [0, 1] range.
+
+    Parameters
+    ----------
+    path : str
+        Path to the jpeg to read
+
+    Returns
+    -------
+        tf tensor in float32
+    """
     image = tf.io.read_file(path)
     return preprocess_image(image)
 
 
 def get_dataset(image_dir, labels_csv):
+    """
+    Prepare the dataset for training. Images are normalized to [0, 1] range. Labels are n-hot encoded.
+
+    Parameters
+    ----------
+    image_dir : str
+        Path to the directory containing the images
+    labels_csv : str
+        Path to the csv file
+
+    Returns
+    -------
+        tf.data.ZipDataset
+            Elements of the dataset are structured (image, labels)
+    """
     samples = []
     with open(labels_csv) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -60,6 +98,19 @@ def get_dataset(image_dir, labels_csv):
 
 
 def get_labels_distribution(labels_csv):
+    """
+    Returns a dict where the keys are the labels and the values are the number of occurences.
+
+    Parameters
+    ----------
+    labels_csv : str
+        Path to the csv file
+
+    Returns
+    -------
+        dict
+    """
+    # Read the csv
     sample_labels = []
     with open(labels_csv) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -68,6 +119,7 @@ def get_labels_distribution(labels_csv):
                 continue
             sample_labels.append(row[1].split(' '))
 
+    # Count labels occurences
     labels = {}
     for sample in sample_labels:
         for label in sample:
