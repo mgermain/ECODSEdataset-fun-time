6d26b6d699fa43737e13e08651a580388a96fdd5
Simon Blackburn <simon.blackburn@mila.quebec>
2019-06-07 16:02:11 -0400

    STY: fix style in argparse fct

--- ecodse_funtime_alpha/train.py
+++ ecodse_funtime_alpha/train.py
@@ -32,28 +32,41 @@ def fit_loop(dataset, model, optimizer, nepoch, batchsize):
 
 def get_args(args):
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--imagepath', default='../../rainforest/fixed-train-jpg/',
+    argparser.add_argument('--imagepath',
+                           default='../../rainforest/fixed-train-jpg/',
                            help='path to image directory')
-    argparser.add_argument('--labelpath', default='../../rainforest/train_v3.csv',
+    argparser.add_argument('--labelpath',
+                           default='../../rainforest/train_v3.csv',
                            help='path to csv file for labels')
-    argparser.add_argument('--seed', default=-1, type=int,
+    argparser.add_argument('--seed',
+                           default=-1,
+                           type=int,
                            help='Set random seed to this number (if >0)')
-    argparser.add_argument('--kernels', default=1, type=int,
+    argparser.add_argument('--kernels',
+                           default=1,
+                           type=int,
                            help='Number of kernels in the CNN')
-    argparser.add_argument('--ksize', default=1, type=int,
+    argparser.add_argument('--ksize',
+                           default=1,
+                           type=int,
                            help='Size of kernels in CNN')
-    argparser.add_argument('--lr', default=0.1, type=float,
+    argparser.add_argument('--lr',
+                           default=0.1,
+                           type=float,
                            help='Learning rate (default=0.1')
-    argparser.add_argument('--nepoch', default=1, type=int,
+    argparser.add_argument('--nepoch',
+                           default=1,
+                           type=int,
                            help='Number of epoch for training')
-    argparser.add_argument('--batchsize', default=32, type=int,
+    argparser.add_argument('--batchsize',
+                           default=32,
+                           type=int,
                            help='batch size (default=32)')
     args = argparser.parse_args(args)
     return args
 
 
 if __name__ == "__main__":
-    tf.enable_eager_execution()
     args = get_args(sys.argv[1:])
     if args.seed > 0:
         tf.random.set_seed(args.seed)
