6d77c6bb70d77f43a52737cef5f93f218e786043
Mathieu Germain <mathieu.germain@gmail.com>
2019-06-07 14:27:29 -0400

    BLD: Upgraded to python 3.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecodse-funtime-alpha',
-    python_requires='~=3.6',
+    python_requires='>3.7',
     version='0.1a',
     description='Super mega happy fun time',
     long_description="Super LONG mega happy fun time",
@@ -12,7 +12,7 @@ setup(
     classifiers=[
         'Development Status :: 1 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ],
     packages=find_packages(exclude=['docs', 'tests']),
     install_requires=['tensorflow==1.13', 'matplotlib', 'numpy', 'comet-ml', 'scikit-learn'],
