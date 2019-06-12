e2886918afdf99465dd22628d3d0fc9029e1904e
Mathieu Germain <mathieu.germain@gmail.com>
2019-06-12 11:05:31 -0400

    BUG: Removed unused requirement in setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
         'Programming Language :: Python :: 3.7'
     ],
     packages=find_packages(exclude=['docs', 'tests']),
-    install_requires=['tensorflow==1.13', 'matplotlib', 'numpy', 'comet-ml', 'scikit-learn', 'pillow'],
+    install_requires=['tensorflow==1.13', 'matplotlib', 'numpy', 'comet-ml', 'scikit-learn'],
     extras_require={'test': ['flake8', 'pytest', 'pytest-cov', 'pillow', 'codecov']}
 )
