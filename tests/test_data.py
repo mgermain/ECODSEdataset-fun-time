e0a24ca19ff78a26cef811016a28d4083541c8d6
Simon Blackburn <simon.blackburn@mila.quebec>
2019-06-12 09:47:19 -0400

    ENH: adding unit tests to test_data. Getting 100% coverage now

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,22 +1,33 @@
 import numpy as np
+from os.path import join
 from PIL import Image
 import pytest
 
 from ecodse_funtime_alpha.data import get_dataset
 from ecodse_funtime_alpha.data import get_labels_distribution
+from ecodse_funtime_alpha.data import load_and_preprocess_image
 
 
 class TestDataset(object):
     @pytest.fixture(autouse=True)
     def mock_files(self, tmpdir):
+        self.img_size = 256
+        self.img_channel = 3
+        self.img_colorpixel = 128
+        self.img_color = 255
+
         p = tmpdir.mkdir("train-jpg").join("train_v2.csv")
         p.write_text("image_name,tags\ntrain_0,haze primary\ntrain_1,agriculture clear primary water", encoding="utf-8")
 
-        img = np.zeros((256, 256, 3), dtype=np.uint8)
-        img[128] = 255
+        img = np.zeros((self.img_size, self.img_size, self.img_channel), dtype=np.uint8)
+        img[self.img_colorpixel] = self.img_color
 
         Image.fromarray(img).save(str(tmpdir.join("train-jpg").join("train_0.jpg")), quality=100)
         Image.fromarray(img).save(str(tmpdir.join("train-jpg").join("train_1.jpg")), quality=100)
+
+    def test_preprocess_image(self, tmpdir):
+        img = load_and_preprocess_image(str(tmpdir.join("train-jpg").join("train_0.jpg")))
+        assert img[self.img_colorpixel].numpy().all() == self.img_color / 255
 
     def test_image(self, tmpdir):
         image_dir = str(tmpdir.join("train-jpg"))
@@ -59,3 +70,14 @@ class TestDataset(object):
         for k, v in labels.items():
             assert k in true_labels
             assert true_labels[k] == v
+
+    def test_missing_image(self, tmpdir, capsys):
+        p = tmpdir.join("train-jpg").join("missing_image.csv")
+        missing_name = "missing"
+        p.write_text(f"image_name,tags\n{missing_name},haze primary\ntrain_1,agriculture clear primary water", encoding="utf-8")
+        image_dir = str(tmpdir.join("train-jpg"))
+        labels_csv = str(p)
+        get_dataset(image_dir, labels_csv)
+        full_name = join(image_dir, f"{missing_name}.jpg")
+        captured = capsys.readouterr()
+        assert captured.out == f"WARNING: {full_name} does not exist\n"
